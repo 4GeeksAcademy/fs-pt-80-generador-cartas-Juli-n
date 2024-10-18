a7391fc74f5589cf79a57c4e7556adb7272c51eb
Julutus <julian94info@gmail.com>
2024-10-18 10:47:14 +0000

    Cartas aleatorias con temporizador

--- src/app.js
+++ src/app.js
@@ -7,5 +7,49 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //valores
+  const valores = [
+    `2`,
+    `3`,
+    `4`,
+    `5`,
+    `6`,
+    `7`,
+    `8`,
+    `9`,
+    `10`,
+    `J`,
+    `Q`,
+    `K`,
+    `A`
+  ];
+  const palos = [`heart`, `spade`, `club`, `diamond`];
+  const simbolos = {
+    heart: `♥`,
+    spade: `♠`,
+    club: `♣`,
+    diamond: `♦`
+  };
+
+  function generarCarta() {
+    const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+    const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+
+    const card = document.querySelector(`#card`);
+    const paloUp = document.querySelector(`#paloUp`);
+    const valoresDiv = document.querySelector(`#valores`);
+    const paloDown = document.querySelector(`#paloDown`);
+    console.log(valorAleatorio);
+
+    paloUp.innerHTML = simbolos[paloAleatorio];
+    valoresDiv.innerHTML = valorAleatorio;
+    paloDown.innerHTML = simbolos[paloAleatorio];
+
+    card.className = `card`;
+    card.classList.add(paloAleatorio);
+  }
+
+  generarCarta();
+
+  setInterval(generarCarta, 2000);
 };
